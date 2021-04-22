f6a34aa94b055107edba7c2683a4fc4769d54e02
김태훈 <kimtaehoon@smilegate.com>
2021-04-23 00:04:33 +0900

    test lint complete

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-import {sum} from './math';
-import './css/base.css';
-import './scss/normalize.scss';
-import seoyeji from './images/seoyeji2.jpeg';
+// import { sum } from "./math";
+import "./css/base.css";
+import "./scss/normalize.scss";
+import seoyeji from "./images/seoyeji2.jpeg";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const img = document.createElement('img');
-    img.src = seoyeji;
-    document.getElementById('main').appendChild(img);
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.createElement("img");
+  img.src = seoyeji;
+  document.getElementById("main").appendChild(img);
 });
 
 // console.log(sum(10, 15));
-console.log('process.env.NODE_ENV =', process.env.NODE_ENV);
-console.log('TWO =', TWO);
-console.log('api.domain =', api.domain);
-const p = new Promise((resolve, reject) => {
-    resolve('111');
+// console.log("process.env.NODE_ENV =", process.env.NODE_ENV);
+// console.log("TWO =", TWO);
+// console.log("api.domain =", api.domain);
+const p = new Promise((resolve) => {
+  resolve("111");
 });
-console.log()
-(function(){})();;;;;;;;
+console.log(p);
+console.log()(function () {})();
